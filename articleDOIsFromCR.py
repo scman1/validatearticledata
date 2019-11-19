808484bce5ae2b3161ffe42c357f51b276274257
scman1 <a_nieva@hotmail.com>
2019-11-19 13:30:19 +0000

    getDOIS for ScholarData from publish or perish

--- articleDOIsFromCR.py
+++ articleDOIsFromCR.py
@@ -10,9 +10,9 @@ cr = Crossref()
 
 
 def getArticleDOIFromCrossref(article_title):
-    res = cr.works(query = article_title, select = ["DOI","title"])
     doi_text=""
     try:
+        res = cr.works(query = article_title, select = ["DOI","title"])
         for recovered in res['message']['items']:
             similarity = similar(recovered['title'][0], article_title)
             #print(recovered['title'][0], similarity)
@@ -27,29 +27,36 @@ def getArticleDOIFromCrossref(article_title):
 
 # open articles csv file
 catalysis_articles = {}
-input_file = 'UKCatalysisHubArticles201910.csv'
-output_file = 'UKCatalysisHubArticles201910Mod.csv'
+input_file = 'ukch_popVT2MRB7.csv'
+output_file = 'ukch_popVT2MRB7Mod.csv'
 with open(input_file, newline='') as csvfile:
      reader = csv.DictReader(csvfile)
      for row in reader:
          catalysis_articles[int(row['Num'])]=row
-         if row['Identifier'] == "":
-            print(row['Title'], row['Identifier'])
+         
 
 
 #fill in the missing identifier
 for cat_art_num in catalysis_articles.keys():
-    if catalysis_articles[cat_art_num]['Identifier'] =="":
-        article_title = catalysis_articles[cat_art_num]['Title']
-        doi_text=getArticleDOIFromCrossref(article_title)
-        catalysis_articles[cat_art_num]['Identifier'] = doi_text
+    article_title = catalysis_articles[cat_art_num]['Title']
+    doi_text=getArticleDOIFromCrossref(article_title)
+    catalysis_articles[cat_art_num]['DOI'] = doi_text
+    catalysis_articles[cat_art_num]['checked'] = 1
+    print(cat_art_num, article_title)
+    
+
+fieldnames = []
+for cat_art_num in list(catalysis_articles.keys()):
+	keys = list(catalysis_articles[cat_art_num].keys())
+	for key in keys:
+		if not key in fieldnames:
+			fieldnames.append(key)
 
 
+    
 #write back to a new csv file
 with open(output_file, 'w', newline='') as csvfile:
-    fieldnames = ['Num', 'Phase', 'P', 'CatalysisTheme', 'ProjectYear', 'Authors', 'Title', 'Journal', 'PublicationDate', 'Identifier']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    
     for cat_art_num in catalysis_articles.keys():
         writer.writerow(catalysis_articles[cat_art_num])
