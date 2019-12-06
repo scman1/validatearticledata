6f64b362d5945f9a4181c7992a375a415cc0be3b
scman1 <a_nieva@hotmail.com>
2019-12-06 17:54:43 +0000

    mapping affiliations to pubs

--- splitAffiliation.py
+++ splitAffiliation.py
@@ -1,7 +1,24 @@
+import csv
+from difflib import SequenceMatcher
+
+def similar(a, b):
+    return SequenceMatcher(None, a,b).ratio()
+
+def get_data(input_file, id_field):
+    csv_data = {}
+    fieldnames=[]
+    with open(input_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if fieldnames==[]:
+                fieldnames=list(row.keys())
+            csv_data[int(row[id_field])]=row
+    return csv_data, fieldnames
+
 def split_and_assign(input_text):
     ret_parsed = {}
-    fields={'a':'ResearchGroup', 'b':'Department','c':'Faculty',
-            'd':'Institution','e':'Address','f':'City','g':'Country'}
+    fields={'a':'ResearchGroup', 'b':'Department','c':'Faculty','d':'Institution',
+            'e':'Address','f':'City','g':'Country','h':'Postcode'}
     if isinstance(input_text, str):
         user_opt = ""
         while True:
@@ -21,7 +38,7 @@ def split_and_assign(input_text):
         elif user_opt == 'b':
             assgnr = ""
             while True:
-                print('Options:\n a) ResearchGroup\n b) Department\n c) Faculty\n d) Institution\n e) Address\n f) City\n g) Country')
+                print('Options:\n a) ResearchGroup\n b) Department\n c) Faculty\n d) Institution\n e) Address\n f) City\n g) Country\n h) Postcode')
                 assgnr = input()
                 print(assgnr)
                 keys = list(fields.keys())
@@ -33,6 +50,109 @@ def split_and_assign(input_text):
                 
     return ret_parsed
 
-text = 'University of Groningen; Engineering and Technology Institute Groningen (ENTEG), Department of Chemical Engineering; Nijenborgh 4 9747 AG Groningen The Netherlands'
+def add_affiliation(affi_text, affi_records):
+    affiliation = split_and_assign(affi_text)
+    new_id = len(affi_records) + 1
+    affi_records[new_id] = affiliation
+    affi_records[new_id]["ID"] = new_id
+    return affi_records, new_id
 
-affiliation = split_and_assign(text)
+##text = 'University of Groningen; Engineering and Technology Institute Groningen (ENTEG), Department of Chemical Engineering; Nijenborgh 4 9747 AG Groningen The Netherlands'
+##affiliation = split_and_assign(text)
+##print(affiliation)
+
+# open the affiliations and new affiliations files
+affi_file = 'UKCHAffiliationsA.csv'
+new_affi_file = 'UKCHAffiliations201911.csv'
+out_affi_file = 'UKCHAffiliationsB.csv'
+out_links_file = 'UKCHAffiliationLinks.csv'
+
+affi_records, affi_fields = get_data(affi_file, 'ID')
+new_affi_records, new_affi_fields = get_data(new_affi_file, 'ID')
+
+links_list={}
+counter = 0
+for na_key in new_affi_records:
+    counter += 1
+    link_data = new_affi = new_affi_records[na_key]
+    new_affi_text = new_affi['affiliations']
+    new_affis = new_affi_text.split('|')
+    for affi_text in new_affis:
+        if affi_text  != "":
+            likely_affis = {}
+            for afi_key in affi_records:
+                affi = affi_records[afi_key]
+                matches = 0
+                for field in affi:        
+                    if affi_text.find(affi[field]) != -1 and field != 'ID' \
+                       and affi[field]!='':
+                        matches +=1
+                if matches > 2:
+                    likely_affis[afi_key]=matches
+            print('***************************************************************')
+            print(affi_text)
+            if len(likely_affis) == 0:
+                print("no matches")
+                affi_records, new_id = add_affiliation(affi_text, affi_records)
+                print('Map to ID', map_id)
+                link_data.pop('affiliations')
+                link_data['AffiliationID'] = new_id
+                links_list[len(links_list)+1] = link_data
+            else:
+                i_aff = 1
+                options = {}
+                for affi_id in likely_affis:
+                    v_aff = str(affi_id)+"| "
+                    for key in affi_records[affi_id]:
+                        if affi_records[affi_id][key] != "":
+                            v_aff += ", "+ affi_records[affi_id][key]
+                    options[i_aff] = v_aff
+                    i_aff += 1
+                options[i_aff] = "split and add"
+                usr_select = "0"
+                while True:
+                    for opt in options:
+                        print(opt, "Map to:", options[opt])
+                    print("Selection:")
+                    usr_select = int(input())
+                    keys = list(options.keys())
+                    if usr_select in keys:
+                        print("Selected:", options[usr_select])
+                        if usr_select == max(options):
+                            affi_records, new_id = add_affiliation(affi_text, affi_records)
+                            print('Adde ID', new_id)
+                            if 'affiliations' in link_data: 
+                                link_data.pop('affiliations')
+                            link_data['AffiliationID'] = new_id
+                            links_list[len(links_list)+1] = link_data
+                        else:
+                            map_id = options[usr_select].split("|")[0]
+                            print('Map to ID', map_id)
+                            if 'affiliations' in link_data:
+                                link_data.pop('affiliations')
+                            link_data['AffiliationID'] = map_id
+                            new_link_id = len(links_list)+1
+                            links_list[new_link_id] = link_data
+                            print(links_list)
+                        break
+                    
+    if counter == 5:
+        break
+
+with open(out_affi_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=affi_fields)
+    writer.writeheader()
+    for afi_num in affi_records.keys():
+        writer.writerow(affi_records[afi_num])
+        
+link_fields = new_affi_fields
+link_fields.remove("affiliations")
+link_fields.append("AffiliationID")
+
+with open(out_links_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=link_fields)
+    writer.writeheader()
+    for link_num in links_list.keys():
+        writer.writerow(links_list[link_num])
+        
+
