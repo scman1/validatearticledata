98c8f5db3c5ea939b29167309d8bd5f490ba6f53
scman1 <a_nieva@hotmail.com>
2019-12-08 17:20:40 +0000

    mapping affiliations 02

--- splitAffiliation.py
+++ splitAffiliation.py
@@ -45,7 +45,7 @@ def split_and_assign(input_text):
                 print(keys)
                 if assgnr in keys:
                     print('assing to:', assgnr, fields[assgnr])
-                    ret_parsed[fields[assgnr]]=input_text
+                    ret_parsed[fields[assgnr]]=input_text.strip()
                     break;
                 
     return ret_parsed
@@ -62,55 +62,77 @@ def add_affiliation(affi_text, affi_records):
 ##print(affiliation)
 
 # open the affiliations and new affiliations files
-affi_file = 'UKCHAffiliationsA.csv'
+affi_file = 'UKCHAffiliationsB.csv'
 new_affi_file = 'UKCHAffiliations201911.csv'
+links_file = "UKCHAffiliationLinks.csv"
 out_affi_file = 'UKCHAffiliationsB.csv'
 out_links_file = 'UKCHAffiliationLinks.csv'
 
 affi_records, affi_fields = get_data(affi_file, 'ID')
 new_affi_records, new_affi_fields = get_data(new_affi_file, 'ID')
+links_list, link_fields = get_data(links_file, 'LinkID')
 
-links_list={}
 counter = 0
 for na_key in new_affi_records:
     counter += 1
+    max_affi_id = links_list[max(links_list)]['ID']
     link_data = new_affi = new_affi_records[na_key]
-    new_affi_text = new_affi['affiliations']
-    new_affis = new_affi_text.split('|')
+    new_affis=[]
+    if int(max_affi_id) < int(new_affi['ID']):
+        new_affi_text = new_affi['affiliations']
+        new_affis = new_affi_text.split('|')
     for affi_text in new_affis:
         if affi_text  != "":
             likely_affis = {}
             for afi_key in affi_records:
                 affi = affi_records[afi_key]
                 matches = 0
+                afi_str = ""
                 for field in affi:        
-                    if affi_text.find(affi[field]) != -1 and field != 'ID' \
+                    if field != 'ID' and affi_text.find(affi[field]) != -1 \
                        and affi[field]!='':
                         matches +=1
                 if matches > 2:
-                    likely_affis[afi_key]=matches
+                    for field in affi:
+                        if field != 'ID' and affi[field]!='':
+                            if afi_str == "":
+                                afi_str += affi[field]
+                            else:
+                                afi_str += ", " + affi[field]
+                        similarity = similar(affi_text, afi_str)
+                        likely_affis[afi_key]=[matches, similarity, afi_str]
             print('***************************************************************')
             print(affi_text)
             if len(likely_affis) == 0:
                 print("no matches")
                 affi_records, new_id = add_affiliation(affi_text, affi_records)
-                print('Map to ID', map_id)
-                link_data.pop('affiliations')
+                print('Map to ID', new_id)
+                if 'affiliations' in link_data:
+                    link_data.pop('affiliations')
                 link_data['AffiliationID'] = new_id
                 links_list[len(links_list)+1] = link_data
             else:
                 i_aff = 1
                 options = {}
+                affi_linked = False
                 for affi_id in likely_affis:
-                    v_aff = str(affi_id)+"| "
-                    for key in affi_records[affi_id]:
-                        if affi_records[affi_id][key] != "":
-                            v_aff += ", "+ affi_records[affi_id][key]
+                    v_aff = str(affi_id) + "|"+ str(likely_affis[affi_id][0]) + str(likely_affis[affi_id][1]) + str(likely_affis[affi_id][2])
                     options[i_aff] = v_aff
                     i_aff += 1
+                    if likely_affis[affi_id][1] > 0.7:
+                        #map automatically
+                        map_id = affi_id
+                        print('Map to ID', map_id)
+                        if 'affiliations' in link_data:
+                            link_data.pop('affiliations')
+                        link_data['AffiliationID'] = map_id
+                        new_link_id = len(links_list)+1
+                        links_list[new_link_id] = link_data
+                        affi_linked = True
                 options[i_aff] = "split and add"
                 usr_select = "0"
-                while True:
+
+                while not affi_linked:
                     for opt in options:
                         print(opt, "Map to:", options[opt])
                     print("Selection:")
@@ -136,7 +158,7 @@ for na_key in new_affi_records:
                             print(links_list)
                         break
                     
-    if counter == 5:
+    if counter == 160:
         break
 
 with open(out_affi_file, 'w', newline='') as csvfile:
@@ -148,11 +170,13 @@ with open(out_affi_file, 'w', newline='') as csvfile:
 link_fields = new_affi_fields
 link_fields.remove("affiliations")
 link_fields.append("AffiliationID")
+link_fields.append("LinkID")
 
 with open(out_links_file, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=link_fields)
     writer.writeheader()
     for link_num in links_list.keys():
+        links_list[link_num]['LinkID'] = link_num
         writer.writerow(links_list[link_num])
         
 
