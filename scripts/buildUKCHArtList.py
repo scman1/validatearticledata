05c8f1ae3f01dc8b3364b8a870f9a81f155b52bb
scman1 <a_nieva@hotmail.com>
2020-10-18 09:22:53 +0100

    get pubs list from UKCH pages 03 - get bag publications

--- scripts/buildUKCHArtList.py
+++ scripts/buildUKCHArtList.py
@@ -39,6 +39,16 @@ def getParagraphs(soup):
         print(e)
     return result
 
+def getListItems(soup):
+    result = []
+    try:
+        for line_item in soup.find_all('li'):
+            if len(line_item.attrs) == 0:
+                result.append(line_item)
+    except Exception as e:
+        print(e)
+    return result
+            
 
 def findFromURI(name, url, referents):
     result = ""
@@ -88,10 +98,11 @@ pub_frags = ['publications', 'biocatalysis-publications-2017/',
             'environment-publications-2016/', 'environment-publications-2017/',
             'environment-publications-2018/', 'transformations-publications-2014/',
             'transformations-publications-2015/', 'transformations-publications-2016/',
-            'transformations-publications-2017/', 'transformations-publications-2018/']
+            'transformations-publications-2017/', 'transformations-publications-2018/',
+            'bag-at-diamond-light-source/']
 
 
-output_file = 'UKCH202010.csv'
+output_file = 'UKCH20201018.csv'
 
 
 catalysis_articles = {}
@@ -99,66 +110,89 @@ single_article = {}
 i = 0
 for pub_frag in pub_frags:
     pub_url = base_url + pub_frag
+    print("Getting: ", pub_frag.replace("-", " ").replace("/", ""))
     proyect_theme = "look_it_up"
     proyect_year = "2020"
-    if pub_frag != 'publications':
+    if pub_frag != 'publications' and 'bag-at-diamond' not in pub_frag:
         proyect_theme = pub_frag.replace('publications-','')
         proyect_year = proyect_theme[-5:-1]
         proyect_theme = proyect_theme[:-6]
+    elif 'bag-at-diamond' in pub_frag:
+        proyect_theme = "Bag"
+
+
+    if proyect_theme == "Bag":
+        parsed_page = getHTMLPage(pub_url)
+        list_of_li = getListItems(parsed_page)
+        item = 0
+        longest = 0
+        for list_item in list_of_li:
+            single_article={}
+            if len(list_item.get_text()) < 1000 and len(list_item.get_text()) > 60:
+                i += 1
+                #get li text (hard to parse)
+                single_article['CatArtNum'] = i
+                single_article['blob'] = list_item.get_text()
+                single_article['theme'] = proyect_theme
+                single_article['p_year'] = proyect_year
+                catalysis_articles[i] = single_article
+                if len(list_item.get_text()) > longest:
+                    longest = len(list_item.get_text())
+    else:
+        parsed_page = getHTMLPage(pub_url)        
+        list_of_links = getLinks(parsed_page) # not being used
+        list_of_paragraphs = getParagraphs(parsed_page)
+        for para in list_of_paragraphs:
+            authors = title = pub_data = link_text = art_ref = ""
+            single_article={}
+            if para.find('strong') and para.find('a'):
+                # standard formating until 2018 on landing page
+                i += 1
+                full_para = para.get_text()
+                title = para.find('strong').get_text()
+                start_title = full_para.index(title)
+                end_title = start_title + len(title) 
+                authors = full_para[0:start_title-2]
+                link_text = para.find('a').get_text()
+                art_ref= para.find('a').get('href')
+                start_link =  full_para.index(link_text)
+                pub_data = full_para[end_title+2:start_link-2]
+                single_article['CatArtNum'] = i
+                single_article['Authors'] = authors
+                single_article['Title'] = title
+                single_article['PubData'] = pub_data
+                single_article['LinkText'] = link_text
+                single_article['ArtRef'] = art_ref
+                single_article['theme'] = proyect_theme
+                single_article['p_year'] = proyect_year
+                catalysis_articles[i] = single_article
+            elif para.find('strong') and i > 80:
+                # non standard formating, title but no link
+                i += 1
+                full_para = para.get_text()
+                title = para.find('strong').get_text()
+                start_title = full_para.index(title)
+                end_title = start_title + len(title) 
+                authors = full_para[0:start_title-2]
+                pub_data = full_para[end_title+2:len(full_para)]
+                single_article['CatArtNum'] = i
+                single_article['Authors'] = authors
+                single_article['Title'] = title
+                single_article['PubData'] = pub_data
+                single_article['LinkText'] = link_text
+                single_article['ArtRef'] = art_ref
+                single_article['theme'] = proyect_theme
+                single_article['p_year'] = proyect_year
+                catalysis_articles[i] = single_article
+            elif i >= 102:
+                i += 1
+                #get non formated on main page (will bring some trash)
+                single_article['CatArtNum'] = i
+                single_article['blob'] = para.get_text()
+                single_article['theme'] = proyect_theme
+                single_article['p_year'] = proyect_year
+                catalysis_articles[i] = single_article
     
-    parsed_page = getHTMLPage(pub_url)
-    list_of_links = getLinks(parsed_page)
-    list_of_paragraphs = getParagraphs(parsed_page)
-    for para in list_of_paragraphs:
-        authors = title = pub_data = link_text = art_ref = ""
-        single_article={}
-        if para.find('strong') and para.find('a'):
-            # standard formating until 2018 on landing page
-            i += 1
-            full_para = para.get_text()
-            title = para.find('strong').get_text()
-            start_title = full_para.index(title)
-            end_title = start_title + len(title) 
-            authors = full_para[0:start_title-2]
-            link_text = para.find('a').get_text()
-            art_ref= para.find('a').get('href')
-            start_link =  full_para.index(link_text)
-            pub_data = full_para[end_title+2:start_link-2]
-            single_article['CatArtNum'] = i
-            single_article['Authors'] = authors
-            single_article['Title'] = title
-            single_article['PubData'] = pub_data
-            single_article['LinkText'] = link_text
-            single_article['ArtRef'] = art_ref
-            single_article['theme'] = proyect_theme
-            single_article['p_year'] = proyect_year
-            catalysis_articles[i] = single_article
-        elif para.find('strong') and i > 80:
-            # non standard formating, title but no link
-            i += 1
-            full_para = para.get_text()
-            title = para.find('strong').get_text()
-            start_title = full_para.index(title)
-            end_title = start_title + len(title) 
-            authors = full_para[0:start_title-2]
-            pub_data = full_para[end_title+2:len(full_para)]
-            single_article['CatArtNum'] = i
-            single_article['Authors'] = authors
-            single_article['Title'] = title
-            single_article['PubData'] = pub_data
-            single_article['LinkText'] = link_text
-            single_article['ArtRef'] = art_ref
-            single_article['theme'] = proyect_theme
-            single_article['p_year'] = proyect_year
-            catalysis_articles[i] = single_article
-        elif i >= 102:
-            i += 1
-            #get non formated on main page (will bring some trash)
-            single_article['CatArtNum'] = i
-            single_article['blob'] = para.get_text()
-            single_article['theme'] = proyect_theme
-            single_article['p_year'] = proyect_year
-            catalysis_articles[i] = single_article
 
 fieldnames = []
 for cat_art_num in list(catalysis_articles.keys()):
