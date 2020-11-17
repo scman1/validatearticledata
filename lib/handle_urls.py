8568400efd85d3af95fd16535cdc08aed5fe39c2
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2020-11-17 15:48:32 +0000

    Availability Score

--- lib/handle_urls.py
+++ lib/handle_urls.py
@@ -1,5 +1,6 @@
 import requests
 import urllib
+import json
 from bs4 import BeautifulSoup
 
 def findFromURI(name, uri, referents):
@@ -87,4 +88,55 @@ def getPageHeader(url_text):
         print(e)
     return response
 
+def getObject(url_text, store_at = "do_files/"):
+    ret_object = {}
+    try:
+        req_head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+        print('trying to recover object from', url_text)
+        response = requests.get(url_text, headers = req_head)
+        if response.status_code == 200:
+            print ('got something back')
+            ret_object['resource_url'] = response.url
+            print ('resource url', ret_object['resource_url'])
+            if 'content-type' in response.headers.keys():
+                ret_object['type'] = response.headers['content-type']
+            if 'content-length' in response.headers.keys():
+                ret_object['size'] = response.headers['content-length']
+            #name file using the url fragment after the last /
+            fname = response.url.split("/")[-1:] 
+            print(fname)
+            ret_object['file_name']  = store_at + fname
+            with open(ret_object['file_name'], 'wb') as f:
+                f.write(response.content)
+    except Exception as e:
+        print(e)
+    finally:
+        return ret_object
 
+# try to get the object's metadata from DOIs
+req_accepts = ['application/x-bibtex', 'application/x-research-info-systems', 'application/vnd.citationstyles.csl+json']
+def getObjectMetadata(url_text, req_accept = 2):
+    ret_object = {}
+    try:
+        req_head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+        req_head['Accept'] = req_accepts[req_accept]
+        print('trying to recover object from', url_text)
+        response = requests.get(url_text, headers = req_head)
+        if response.status_code == 200:
+            print ('got something back')
+            ret_object['resource_url'] = response.url
+            print ('resource url', ret_object['resource_url'])
+            if 'content-type' in response.headers.keys():
+                ret_object['type'] = response.headers['content-type']
+            if 'content-length' in response.headers.keys():
+                ret_object['size'] = response.headers['content-length']
+            #name file using the url fragment after the last /
+            if req_accept == 2:
+                contents_json = json.loads(response.content.decode())
+                ret_object['metadata'] = contents_json
+            else:
+               ret_object['metadata'] = contents.decode()
+    except Exception as e:
+        print(e)
+    finally:
+        return ret_object
